32088d3b34b15717921fb093329aa37702d7af52
Bruno Horta <brunohorta@Brunos-MacBook-Pro.local>
2017-11-06 20:03:06 +0000

    no message

---
+++ cam.py
@@ -0,0 +1,27 @@
+import cv2
+import urllib 
+import numpy as np
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+stream=urllib.urlopen('http://10.20.229.7/stream')
+bytes=''
+while True:
+    bytes+=stream.read(1024)
+    a = bytes.find('\xff\xd8')
+    b = bytes.find('\xff\xd9')
+    if a!=-1 and b!=-1:
+        jpg = bytes[a:b+2]
+        bytes= bytes[b+2:]
+        img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),1)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+            img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        cv2.imshow('i',img)
+        if cv2.waitKey(1) ==27:
+            exit(0) 
